16f936ba055b169042211dab0b046701b99a5380
khris22 <khrispunzalan@gmail.com>
2020-08-31 07:16:14 -0500

    Display result on html

--- index.js
+++ index.js
@@ -18,3 +18,22 @@ function findMatches(wordsToMatch, cities) {
     })
 }
 
+// display the result
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        return `
+            <li>
+                <span class='name'>${place.city}, ${place.state}</span>
+                <span class='population'>${place.populstion}</span>
+            </li>
+        `
+    }).join('')
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches)
+searchInput.addEventListener('keyup', displayMatches)
